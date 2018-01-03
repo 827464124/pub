34bd40c08505dcc18adc54703fc90b08bdd3e5de
zh_wenxing <zh_wenxing@163.com>
2018-01-03 07:05:05 -0800

    20180103

--- perl/interface/data.pl
+++ perl/interface/data.pl
@@ -11,13 +11,13 @@ $ua->timeout(10);
 
 
 #&get_info ('sh601009');
-&get_info ('00001');
+&get_info ('sh600756');
 sub get_info 
 {
 
 	my $data_name=shift;
 	my $app_key="cb59fe4800ea015921b3b0b354db7ab9";
-	my $data_interface='http://web.juhe.cn:8080/finance/stock/hk?num='.$data_name.'&key='.$app_key.'&type=0';
+	my $data_interface='http://web.juhe.cn:8080/finance/stock/hs?gid='.$data_name.'&key='.$app_key;
 	
 	my $result="";
 	my $data_hash="";
@@ -30,10 +30,29 @@ sub get_info
 	}
 
 	$result =~ s/:/=>/g;
-print $result;
+#print $result;
 	$data_hash = eval $result;
 
 print $data_hash->{'error_code'};
-	Dumper($data_hash);
+	print Dumper($data_hash);
+	my $data_info = $data_hash->{'result'}[0];
+
+	print "买一：  价格：$data_info->{'data'}{'buyOnePri'}       \t数量： $data_info->{'data'}{'buyOne'}\n";
+	print "买二：  价格：$data_info->{'data'}{'buyTwoPri'}       \t数量： $data_info->{'data'}{'buyTwo'}\n";
+	print "买三：  价格：$data_info->{'data'}{'buyThreePri'}     \t数量： $data_info->{'data'}{'buyThree'}\n";
+	print "买四：  价格：$data_info->{'data'}{'buyFourPri'}      \t数量： $data_info->{'data'}{'buyFour'}\n";
+	print "买五：  价格：$data_info->{'data'}{'buyFivePri'}      \t数量： $data_info->{'data'}{'buyFive'}\n";
+print "===========================================================================================\n";
+	print "卖一：  价格：$data_info->{'data'}{'sellOnePri'}      \t数量： $data_info->{'data'}{'sellOne'}\n";
+	print "卖二：  价格：$data_info->{'data'}{'sellTwoPri'}      \t数量： $data_info->{'data'}{'sellTwo'}\n";
+	print "卖三：  价格：$data_info->{'data'}{'sellThreePri'}    \t数量： $data_info->{'data'}{'sellThree'}\n";
+	print "卖四：  价格：$data_info->{'data'}{'sellFourPri'}     \t数量： $data_info->{'data'}{'sellFour'}\n";
+	print "卖五：  价格：$data_info->{'data'}{'sellFivePri'}     \t数量： $data_info->{'data'}{'sellFive'}\n";
 
 }
+
+
+
+
+
+
